6350a3798e187c942db6f831200fa87fb98973ff
PRIEST099 <ahadic044@gmail.com>
2023-06-20 15:55:47 -0700

    advanced tasks

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - printing multiples of 3 and 5 in 1024
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	in i, sum;
+
+	i = 1;
+	sum = 0;
+	while (i <1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum +=i;
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
