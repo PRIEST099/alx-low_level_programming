90fa220ea75fae3b8b3cf14628c68b274baba40b
PRIEST099 <ahadic044@gmail.com>
2023-06-21 03:58:39 -0700

    104-fibbonacci series

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,20 +1,21 @@
-#include<stdion>
+#include<stdio.h>
 /**
  * main - prints the sum of fibbonacci seriews
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int a, b, i, temp;
+	long a, b, temp;
+	int i;
 
 	a = 1;
 	b = 2;
 	i = 2;
-	printf("%d, %d", a, b);
+	printf("%ld, %ld", a, b);
 	while (i < 98)
 	{
 		temp = a + b;
-		printf(", %d", temp);
+		printf(", %ld", temp);
 		a = b;
 		b = temp;
 		i++;
