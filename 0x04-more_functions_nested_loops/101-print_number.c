0f667686d3d5a4c4e60597768b9be3ca42392ba6
PRIEST099 <ahadic044@gmail.com>
2023-06-23 19:25:24 -0700

    101-print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,26 +2,27 @@
 
 /**
  * print_number - prints an integer
- * @n: integer to be printed
+ * @n: parameter passed to be printed
+ * Return: null
  */
+
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int real;
 
 	if (n < 0)
 	{
-		n1 = -n;
+		real = -n;
 		_putchar('-');
-	} else
+	}
+	else
 	{
-		n1 = n;
+		real = n;
 	}
 
-	if (n1 / 10)
+	if (real / 10)
 	{
-		print_number(n1 / 10);
+		print_number(real / 10);
 	}
-
-	_putchar((n1 % 10) + '0');
+	_putchar((real % 10) + '0');
 }
-
