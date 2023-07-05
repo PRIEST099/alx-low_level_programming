e2d8c101bfcdf9e259e276cf05b4086b669dc250
PRIEST099 <ahadic044@gmail.com>
2023-07-05 11:37:58 -0700

    0-memset changed while loop to for loop

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,17 +11,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	if ( s[i] = '\' || n = 0)
+	for (i - 0; i < n; i++)
 	{
-		return (s);
-	}
-
-	while (i < n)
-	{
-	       (s + i) = b;
-		i++;
+		s[i] = b;
 	}
 	return (s);
+
 }
