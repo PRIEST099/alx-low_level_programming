cffc5c4885d5b3d50bd027d7d0fae07c3439e3a1
PRIEST099 <ahadic044@gmail.com>
2023-07-05 11:10:41 -0700

    0-memset initial commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_memset - sets a constant byte to memory like memset
+ * @s: parameter string to be modified
+ * @b: cahracter to be used
+ * @n: number of elements to be changed
+ * Author: ahadi
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	if ( s[i] = '\' || n = 0)
+	{
+		return (s);
+	}
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
