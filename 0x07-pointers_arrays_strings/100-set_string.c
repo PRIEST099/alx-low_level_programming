80c5ab9ecab811dbd4018d2df3c6410237bc5724
PRIEST099 <ahadic044@gmail.com>
2023-07-05 15:28:59 -0700

    My primary goal of hacking was the intellectual curiosity, the seduction of adventure

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets string to pointer to string pointer
+ * @s: pointer to be modified
+ * @to: pointer parameter to set
+ * Author: Ahadi
+ * Return: null
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
