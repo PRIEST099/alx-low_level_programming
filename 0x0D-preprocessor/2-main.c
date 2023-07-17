5bc5bb7fd59fccab42b7099caebdd7494a7c9d08
PRIEST099 <ahadic044@gmail.com>
2023-07-16 23:31:34 -0700

    mandatory tasks initial commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a program to print a file name fo source code
+ * Return: integer success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
