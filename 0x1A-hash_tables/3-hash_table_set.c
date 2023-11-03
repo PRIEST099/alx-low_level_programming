b0384aa6ec10d6137735fb9e2163434a8f15c319
Ahadi <ahadic044@gmail.com>
2023-11-03 13:26:52 -0700

    changed the workflow of 3-setting hashtable

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,25 +13,25 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
-	char *value_cpy;
 	unsigned long int idx, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	value_cpy = key_index((const unsigned char *)key, ht->size);
-	for (i = idx; ht->array[i]; i++)
+	idx = key_index((const unsigned char *)key, ht->size);
+	for (i = 0; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = value_cpy;
+			ht->array[i]->value = strdup(value);
+			if (ht->array[i]->value == NULL)
+				return (0);
 			return (1);
 		}
 	}
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
-		free(value_cpy);
 		return (0);
 	}
 	new->key = strdup(key);
@@ -40,7 +40,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new);
 		return (0);
 	}
-	new->value = value_cpy;
+	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (0);
+	}
 	new->next = ht->array[idx];
 	ht->array[idx] = new;
 	return (1);
