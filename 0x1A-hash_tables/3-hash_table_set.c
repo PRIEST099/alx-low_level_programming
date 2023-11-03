1557475cb14abae67b32f1f75457b83c0c0213c1
Ahadi <ahadic044@gmail.com>
2023-11-03 12:57:51 -0700

    changing the directory

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *value_cpy;
+	unsigned long int idx, i;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+	value_cpy = key_index((const unsigned char *)key, ht->size);
+	for (i = idx; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_cpy;
+			return (1);
+		}
+	}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_cpy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_cpy;
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
+	return (1);
+}
